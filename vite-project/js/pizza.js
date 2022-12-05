da5c1842c06097fec007848f43b7a740dff25c43
KatieLee1109 <113190058+KatieLee1109@users.noreply.github.com>
2022-12-05 10:15:26 -0500

    Commit

---
+++ vite-project/js/pizza.js
@@ -0,0 +1,10 @@
+const pizza = [
+    {
+        name: "Capri Pizza",
+        info: "Includes Mozzarella Cheese with fresh basil and fresh tomato",
+        price: 25,
+        img = ""
+
+
+    }
+]
