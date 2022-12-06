4ed36f0f91f18152ae803986350c7fc1e1682a8d
KatieLee1109 <113190058+KatieLee1109@users.noreply.github.com>
2022-12-06 10:16:37 -0500

    Commit

--- vite-project/js/pizza.js
+++ vite-project/js/pizza.js
@@ -1,10 +1,31 @@
 const pizza = [
     {
-        name: "Capri Pizza",
+        name: "Caprese Pizza",
         info: "Includes Mozzarella Cheese with fresh basil and fresh tomato",
         price: 25,
-        img = ""
-
-
+        img = "https://cdn.loveandlemons.com/wp-content/uploads/2019/09/margherita-pizza.jpg"
+    },
+    {
+        name: "Pepperoni Pizza",
+        info: "Includes fresh tomato sauce with Mozzarella Cheese and freshly made pepperoni",
+        price: 30,
+        img = "https://www.theimpulsivebuy.com/wordpress/wp-content/uploads/2022/01/phspicylover1.jpeg"
+    },
+    {
+        name: "Hawaiian Pizza",
+        info: "Includes pineapple with Mozzarella Cheese and bacon",
+        price: 30,
+        img: "https://images.thebrag.com/tb/uploads/2022/08/pizza-hut-hawaiian-pizza-768x494.jpg"
+    },
+    {
+        name: "Veggie Pizza",
+        info: "Includes Mozzarella Cheese, tomato sauce, bell peppers and olives",
+        price: 35,
+        img: "https://www.secondrecipe.com/wp-content/uploads/2021/10/air-fryer-pizza-1.jpg"
+    },
+    {
+        name: "Chicago Deep Dish Pizza",
+        info: "Includes "
     }
+
 ]
