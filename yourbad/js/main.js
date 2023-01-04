852c9a945f68a7551f88f909f740f8c328375c2d
KatieLee1109 <113190058+KatieLee1109@users.noreply.github.com>
2023-01-04 10:12:53 -0500

    Commit

--- yourbad/js/main.js
+++ yourbad/js/main.js
@@ -6,27 +6,7 @@ console.log(DOMSelectors);
 
 function makemenu() {
     pizza.forEach((pizza) =>
-        DOMSelectors.main.insertAdjacentHTML
-            ("beforend",
-                `
-        <div id= "makecard" class="makecard">
-        <h2>${pizza.name}</h2>
-        <p><img src=${pizza.img}</p>
-        <p id="outputText">Price: ${pizza.price}</p>
-        <p id="outputText"> Info: ${pizza.info}</p>
-        <button id="buy">Choose</button>
-        </div>
-        `
-            ))
-};
-
-
-
-
-
-
-
-
-
-
-
+        DOMSelectors.main.insertAdjacentHTML(
+            "beforend"
+                ``
+        )
